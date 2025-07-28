3aac572c5a868e5491766c1f3525a0f7fc271f26
Serhii Kupriienko <skupriienko@anaconda.com>
2025-07-28 17:56:25 +0300

    Add headers

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,11 @@ set -x
 
 if [[ ${target_platform} == osx-* ]]; then
   DISABLE_MACOS_FRAMEWORK=--disable-macos-framework
+  # Enable GSSAPI on macOS - libkrb5 now includes all necessary headers
+  GSSAPI_OPTION="--enable-gssapi"
+else
+  # Enable GSSAPI on Linux - libkrb5 now includes all necessary headers
+  GSSAPI_OPTION="--enable-gssapi"
 fi
 
 # Cyrus sasl REALLY wants something called gcc to exist.  Desperately
@@ -17,7 +22,7 @@ autoreconf -vfi
 ./configure --prefix=${PREFIX}                    \
             --host=${HOST}                        \
             ${BUILD_FLAG}                         \
-            ${GSSAPI}                             \
+            ${GSSAPI_OPTION}                       \
             --enable-digest                       \
             --with-des=${PREFIX}                  \
             --with-plugindir=${PREFIX}/lib/sasl2  \
